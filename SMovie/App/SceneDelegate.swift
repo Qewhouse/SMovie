e3725f84d1b583bade1ffaa5855fdee2633b0e10
Alexander Altman <qewhouse@gmail.com>
2023-04-01 21:12:51 +0300

    initial commit

---
+++ SMovie/App/SceneDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  SceneDelegate.swift
+//  SMovie
+//
+//  Created by Alexander Altman on 01.04.2023.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    var window: UIWindow?
+
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let _ = (scene as? UIWindowScene) else { return }
+    }
+
+}
+
