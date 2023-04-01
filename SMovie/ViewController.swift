88591c4a8bc0c26728f4f21569a6c51c043e5f7d
Alexander Altman <qewhouse@gmail.com>
2023-04-01 21:03:37 +0300

    Initial Commit

---
+++ SMovie/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SMovie
+//
+//  Created by Alexander Altman on 01.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
